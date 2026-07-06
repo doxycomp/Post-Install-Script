0ca18bf58164121181d63ec1d292e66907fa320c
Maxii <PC.Maxi@outlook.de>
2026-07-06 16:22:48 +0200

    Updated PostInstall.py to now feature a small window for testing purposes
    Made a small obbject oriented program to test if I can call it with my previous scripts

--- PostInstall.py
+++ PostInstall.py
@@ -1,1 +1,26 @@
+import tkinter as tk
+
 print("Hello World")
+
+class PostInstallerApp(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        
+        # 1. Hauptfenster konfigurieren
+        self.title("Post Installer")
+        self.geometry("1200x800")
+        
+        # 2. Widgets initialisieren
+        self.create_widgets()
+
+    def create_widgets(self):
+        # Alle UI-Elemente werden hier definiert und an 'self' gebunden
+        self.label1 = tk.Label(self, text="Welcome to the Post Installer!")
+        self.label1.pack()
+
+# Script-Einstiegspunkt
+if __name__ == "__main__":
+    app = PostInstallerApp()
+    app.mainloop()
+
+print("Programm finished")
